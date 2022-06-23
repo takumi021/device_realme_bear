2ee07257be7c027f778f0cda74db0fd4adda2720
TheMalachite <eliasgheeraert@gmail.com>
2022-06-23 07:21:49 +0200

    r5x: Drop prebuilt Health service
     * We can just build AOSP one while shipping OPPO one, also downgrad build target to build 2.0 one because of OPPO one

--- device.mk
+++ device.mk
@@ -207,8 +207,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl-qti \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
