940ae32f6f3c3968e6ae98b396865a57abc22f49
takumi021 <omrameshpatel@gmail.com>
2022-08-14 20:57:19 +0000

    bear: Setup Prebuilt Fingerprint

--- device.mk
+++ device.mk
@@ -158,9 +158,6 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
 # Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.bear
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
